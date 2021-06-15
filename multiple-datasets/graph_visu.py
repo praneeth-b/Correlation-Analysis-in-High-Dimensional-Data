31ea1a2212860681615862db9ec9e8c0fd5333ac
Praneeth <balakrishna.praneeth@gmail.com>
2021-06-15 16:38:12 +0200

    visualization as a graph, per signal

--- multiple-datasets/graph_visu.py
+++ multiple-datasets/graph_visu.py
@@ -1,9 +1,10 @@
 import networkx as nx
 import matplotlib.pyplot as plt
+import numpy as np
 
 
 class visualization(object):
-    def __init__(self, corr_estimate, x_corr, num_signals, num_dataset):
+    def __init__(self, corr_estimate_pairwise, corr_estimate,  x_corr, num_signals, num_dataset, type=1):
         """
 
         Args:
@@ -11,13 +12,15 @@ class visualization(object):
             x_corr ():
             num_signals ():
         """
-        self.graph_matrix = corr_estimate
+        self.graph_matrix = corr_estimate_pairwise
+        self.typ2graph_matrix = corr_estimate
         self.edge_list = x_corr
         self.signals = num_signals
         self.num_nodes = num_dataset
+        self.type = type
 
-    def create_subplots(self):
-        fig, axes = plt.subplots(4, 2)
+    def create_subplots(self, shape):
+        fig, axes = plt.subplots(*shape)
         self.axes = axes.flatten()
 
     def create_graph_nodes(self):
@@ -29,18 +32,61 @@ class visualization(object):
             for j in range(len(self.edge_list)):
                 if self.graph_matrix[i, j] == 1:
                     g_list[i].add_edge(*self.edge_list[j])
-            self.axes[i].set_title('signal '+str(i+1))
-            nx.draw_networkx(g_list[i], with_labels=True, font_weight='bold', ax=self.axes[i])
-
+            # pos = nx.random_layout(g_list[i], seed=89)
+            pos = nx.circular_layout(g_list[i], scale=0.8)
+            #print(pos)
+            self.axes[i].set_title('signal ' + str(i + 1))
+            self.axes[i].set_xlim([-1.25, 1.25])
+            self.axes[i].set_ylim([-1.25, 1.25])
+            nx.draw_networkx(g_list[i], pos=pos, with_labels=True, font_weight='bold', ax=self.axes[i])
+        if self.signals % 2 > 0:
+            self.axes[-1].set_visible(False)
         plt.show()
+
+    def generate_coordinates(self, node_list, datasets=1):
+        y = np.linspace(1, -1, self.signals + 1)
+        x = np.linspace(-0.9, 0.9, self.num_nodes)
+        offset_x = np.linspace(-0.9, 0,9, self.num_nodes)
+
+        #xy = map(list, zip(x, y))
+        #pos = dict(zip(range(self.signals + 1), xy))
+        pos = []
+        for xi in x:
+            px = [xi]*(self.signals+1)
+            xy = map(list, zip(px, y))
+            pos.append(dict(zip(range(self.signals + 1), xy)))
+
+        return pos
+
+
+
+
+        return pos
+
+    def create_type2_graph(self):
+        node_list = list(range(self.signals))
+        g_list = [None] * self.num_nodes
+        #glist = nx.Graph()
+        #glist.add_nodes_from(node_list)
+
+        pos_all = self.generate_coordinates(node_list, 1)
+
+        for i in range(self.num_nodes):
+            g_list[i] = nx.Graph()
+            g_list[i].add_nodes_from(node_list)
+
+
+
 
 
     def visualize(self):
-        self.create_subplots()
-        self.create_graph_nodes()
+        if self.type == 1:
+            self.create_subplots((int(np.ceil(self.signals / 2)), 2))
+            self.create_graph_nodes()
 
-
-
+        if self.type == 2:
+            self.create_subplots((self.signals, 1))
+            self.create_type2_graph()
 
 
 # g = nx.Graph()
